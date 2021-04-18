df9dac00b93b39be1a0b66ab2c3bacd37d8a0815
Ricardo <miranda.prado.ricardo@gmail.com>
2021-04-18 10:47:06 -0600

    Code corrections, commentaries, and references to find the libraries used.

--- CrashSavers.ino
+++ CrashSavers.ino
@@ -1,17 +1,26 @@
 // The following code was developed by a joint venture between Desarrollos RM and Digital Strings
+
+// References
+// For the Pressure sensor: https://makersportal.com/blog/2020/6/4/mps20n0040d-pressure-sensor-calibration-with-arduino
+// For the Bluetooth module: https://www.aranacorp.com/en/arduino-and-bluetooth-module-hc-06/
+
 // Libraries
 #include <SoftwareSerial.h>
+#include <Q2HX711.h> // Search for Queuetue HX711 Library
 
 // Define PINs to be used
+
+// Bluetooth
 int RX = 2; //Arduino new RX
 int TX = 3; //Arduino new TX
 
-int pin_sen_1 = A0; //Sensor 1
+// Sensor pins to use
+int out_sen_1 = A0; //Sensor 1
 int sdk_sen_1 = 4; //SDK from Sensor 1
-
-int pin_sen_2 = A1; //Sensor 2
+int out_sen_2 = A1; //Sensor 2
 int sdk_sen_2 = 5; //SDK from Sensor 1
 
+// LED´s
 int led_r = 6; //Red LED
 int led_g = 7; //Green LED
 int led_b = 8; //Blue LED
@@ -22,7 +31,7 @@ SoftwareSerial hc06(RX,TX);
 
 // Variables
 float diff_sen = 0; // Sensors pressure difference 
-float diff_val = 20; // Predefined value for pressure difference
+float diff_val = 20; // Predefined value for pressure difference wanted
 
 
 void setup() {
@@ -60,8 +69,8 @@ float get_diff (float press_diff) {
 
     // Space to get the sensors values
     
-    float a = analogRead(pin_sen_1); //get analog read from sensor 1
-    float b = analogRead(pin_sen_2); //get analog read from sensor 2
+    float a = analogRead(out_sen_1); //get analog read from sensor 1
+    float b = analogRead(out_sen_2); //get analog read from sensor 2
     
     // --------------------------------
     
