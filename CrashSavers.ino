74a9581bd17e12ed2e0424ad6083fceea139e71b
Ricardo <miranda.prado.ricardo@gmail.com>
2021-05-01 13:17:45 -0600

    Sensores de Presión Funcionales en kPa

--- CrashSavers.ino
+++ CrashSavers.ino
@@ -16,9 +16,9 @@ int RX = 2; //Arduino new RX
 int TX = 1; //Arduino new TX
 
 // Sensor pins to use
-const byte out_sen_1 = 4; //Sensor 1
+const byte out_sen_1 = 2; //Sensor 1
 const byte sdk_sen_1 = 3; //SDK from Sensor 1
-const byte out_sen_2 = 6; //Sensor 2
+const byte out_sen_2 = 4; //Sensor 2
 const byte sdk_sen_2 = 5; //SDK from Sensor 1
 
 Q2HX711 sen_1(out_sen_1, sdk_sen_1); 
@@ -45,17 +45,33 @@ void setup() {
   pinMode(led_b, OUTPUT);
 
   //Initialize Bluetooth Serial Port
-  hc06.begin(9600);
+  //hc06.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-
-  diff_sen = get_diff(diff_val); // Get pressure difference
   
-  while(hc06.available()>0){
-    send_diff(diff_sen); //Send both sensor data to the app
-  }
-  
+    float a = sen_1.read(); //get analog read from sensor 1
+    //float vr_a = (a*5.0)/(128.0*pow(2,24.0)-1.0);
+    float p_a = map(a,8388608, 16777215, 0, 40);//(29.5/50)*(vr_a-22.6);
+    Serial.print("Sensor 1: ");
+    Serial.print(p_a,4);
+    Serial.println("kPa");
+    delay(1000);
+    float b = sen_2.read(); //get analog read from sensor 2
+    //float vr_b = (b*5.0)/(128.0*pow(2,24.0)-1.0);
+    float p_b = map(b,8388608, 16777215, 0, 40);//(29.5/50)*(vr_b-22.6);
+    Serial.print("Sensor 2: ");
+    Serial.print(p_b, 4);
+    Serial.println("kPa");
+    delay(1000);    
+    // ----------
+//  diff_sen = get_diff(diff_val); // Get pressure difference
+//  
+//  while(hc06.available()>0){
+//    send_diff(diff_sen); //Send both sensor data to the app
+//  }
+//  
 } 
 
 //Send the pressure value to the app via bluetooth  
@@ -74,8 +90,10 @@ float get_diff (float press_diff) {
     // Space to get the sensors values
     
     float a = sen_1.read(); //get analog read from sensor 1
+    Serial.println(a);
     delay(50);
     float b = sen_2.read(); //get analog read from sensor 2
+    Serial.println(b);
     delay(50);    
     // --------------------------------
     
