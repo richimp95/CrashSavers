fdccfaa713537fff024fe6a38097212b47a33a5d
Ricardo <miranda.prado.ricardo@gmail.com>
2021-05-11 18:09:27 -0600

    Init base measure module created

--- CrashSavers.ino
+++ CrashSavers.ino
@@ -12,8 +12,8 @@
 // Define PINs to be used
 
 // Bluetooth
-int RX = 2; //Arduino new RX
-int TX = 1; //Arduino new TX
+int RX = 10; //Arduino new RX
+int TX = 11; //Arduino new TX
 
 // Sensor pins to use
 const byte out_sen_1 = 2; //Sensor 1
@@ -35,8 +35,9 @@ SoftwareSerial hc06(RX,TX);
 
 // Variables
 float diff_sen = 0; // Sensors pressure difference 
-float diff_val = 30; // Predefined value for pressure difference wanted 100 mm Hg
-
+float diff_val = 90; // Predefined value for pressure difference wanted 90 mm Hg
+float base = 0; // Base value for pressure difference 
+int avg_size = 10;
 
 void setup() {
   // Outputs
@@ -47,6 +48,9 @@ void setup() {
   //Initialize Bluetooth Serial Port
   //hc06.begin(9600);
   Serial.begin(9600);
+
+  base = init_measure();
+  
 }
 
 void loop() {
@@ -55,17 +59,18 @@ void loop() {
   diff_sen = get_diff(diff_val); // Get pressure difference
   Serial.print(diff_sen);
   Serial.println("mmHg");
-//  
+ 
 //  while(hc06.available()>0){
 //    send_diff(diff_sen); //Send both sensor data to the app
 //  }
-//  
+ 
 } 
 
 //Send the pressure value to the app via bluetooth  
 void send_diff(float a) {
     // Here is a space to modify the result depending on the app developer’s requirements.
     hc06.print(a);
+    
 }
 
 // Function to read sensor pins and get the pressure difference
@@ -76,16 +81,16 @@ float get_diff (float press_diff) {
     //Serial.print("Sensor 1: ");
     //Serial.print(p_a,4);
     //Serial.println("mmHg");
-    //delay(500);
+    delay(50); // delay between readings 
     
     float b = sen_2.read(); //get analog read from sensor 2
     float p_b = map(b,8388608, 16777215, 0, 40)*7.5;//map de sensor read to kPa
     //Serial.print("Sensor 2: ");
     //Serial.print(p_b, 4);
     //Serial.println("mmHg");
-    delay(500);    
+    delay(50);  // delay between readings   
     
-    float diff = p_a - p_b; // Difference between sensor 1 and sensor 2
+    float diff = p_a - p_b - base; // Difference between sensor 1 and sensor 2
 
     // If the sensor diff is higher than the pressure we want, LED green is turn ON. Else it turns OFF.
     if (diff >= press_diff){
@@ -98,10 +103,34 @@ float get_diff (float press_diff) {
         digitalWrite(led_g, LOW);
         digitalWrite(led_r, HIGH);
         
-        }
-
-    // Here is a space to modify the result depending on the app developer’s requirements.
+      }
     
     return diff;
 
   }
+
+  float init_measure () {
+   
+    float avg_val = 0.0; // variable for averaging
+    
+    while ( map(sen_1.read(),8388608, 16777215, 0, 40)*7.5 < 80.0 )
+    
+    for (int ii=0;ii<avg_size;ii++){
+
+      float temp_a = sen_1.read(); //get analog read from sensor 1
+      float temp_p_a = map(temp_a,8388608, 16777215, 0, 40)*7.5;//map de sensor read to kPa
+      delay(50); // delay between readings
+      
+      float temp_b = sen_2.read(); //get analog read from sensor 2
+      float temp_p_b = map(temp_b,8388608, 16777215, 0, 40)*7.5;
+      delay(50); // delay between readings
+      
+      avg_val += temp_p_a - temp_p_b; // add multiple ADC readings
+
+    }
+
+    avg_val /= avg_size;
+
+    return avg_val;
+    
+    }
